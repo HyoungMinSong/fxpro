7e460e60d02931d1b134902aa42dd7f71b7945ca
shmin <shmin@BOOK-UF6VE41J48>
2023-03-29 10:47:42 +0900

    main page 초안

---
+++ src/myMain/Main.java
@@ -0,0 +1,35 @@
+package myMain;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("loginForm2.fxml"));
+		System.out.println(loader);
+		Parent loginForm = loader.load();
+		
+//		Opener opener = new Opener();
+//		opener.setPrimaryStage(primaryStage);
+//		LoginController loginCon = loader.getController();
+//		loginCon.setOpener(opener);
+		
+		Scene scene = new Scene(loginForm);
+		primaryStage.setTitle("로그인 화면");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
+
+
+
+
