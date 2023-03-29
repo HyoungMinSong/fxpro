9a9291151915183d64c1db5e7899102afe206d80
KGITBANK509_24 <KGITBANK509_24@DESKTOP-QQFGUBU>
2023-03-29 14:02:24 +0900

    pay

---
+++ src/pay/payDTO.java
@@ -0,0 +1,30 @@
+package pay;
+
+public class payDTO {
+	private String memberId;
+	private int memberTime;
+	private int price;
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public int getMemberTime() {
+		return memberTime;
+	}
+	public void setMemberTime(int memberTime) {
+		this.memberTime = memberTime;
+	}
+	
+	
+}
+
+	
+	
